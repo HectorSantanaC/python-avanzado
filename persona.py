e8fac428f06b2915faa257d1bc203d3dbc344035
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-14 12:43:33 +0100

    Creación de objetos

--- persona.py
+++ persona.py
@@ -8,3 +8,12 @@ class Persona:
     print(f'''Persona:
           Nombre: {self.nombre}
           Apellido: {self.apellido}''')
+
+if __name__ == '__main__':
+  persona1 = Persona()
+  persona1.inicializar_persona('Layla', 'Acosta')
+  persona1.mostrar_persona()
+
+  persona2 = Persona()
+  persona2.inicializar_persona('Ian', 'Sánchez')
+  persona2.mostrar_persona()
