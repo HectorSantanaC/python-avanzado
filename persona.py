44cf81bd595912d0e204080e584a0c54927aada6
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-16 19:39:33 +0100

    Manejo de la memoria en Python

--- persona.py
+++ persona.py
@@ -9,11 +9,17 @@ class Persona:
     print(f'''Persona:
           Nombre: {self.nombre}
           Apellido: {self.apellido}''')
+    print(f'Dir. mem self: {id(self)}')
+    print(f'Dir. mem hex self: {hex(id(self))}')
 
 # Creación de objetos
 if __name__ == '__main__':
   persona1 = Persona('Layla', 'Acosta')
   persona1.mostrar_persona()
+  print(f'Dir. mem persona1: {id(persona1)}')
+  print(f'Dir. mem hex persona1: {hex(id(persona1))}')
 
   persona2 = Persona('Ian', 'Sánchez')
   persona2.mostrar_persona()
+  print(f'Dir. mem persona1: {id(persona2)}')
+  print(f'Dir. mem hex persona1: {hex(id(persona2))}')
