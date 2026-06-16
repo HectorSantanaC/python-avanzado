b06b3dc56eb253585a97dc7a3113bcf3d2a75cb8
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-16 20:18:36 +0100

    Más de constructores en Python

--- aritmetica.py
+++ aritmetica.py
@@ -1,6 +1,6 @@
 class Aritmetica:
 
-  def __init__(self, operando1, operando2):
+  def __init__(self, operando1=None, operando2=None):
     self.operando1 = operando1
     self.operando2 = operando2
 
@@ -15,8 +15,25 @@ class Aritmetica:
 # Programa principal
 if __name__ == '__main__':
   print('*** Ejemplo Clase Aritmetica ***')
+  print('Primer objeto')
   aritmetica1 = Aritmetica(5, 7)
   aritmetica1.sumar()
 
+  print('Segundo objeto')
   aritmetica2 = Aritmetica(12, 16)
   aritmetica2.restar()
+
+  print('Tercer objeto')
+  aritmetica3 = Aritmetica(4)
+  aritmetica3.operando2 = 7
+  aritmetica3.sumar()
+
+  print('Cuarto objeto')
+  aritmetica4 = Aritmetica()
+  aritmetica4.operando1 = 2
+  aritmetica4.operando2 = 6
+  aritmetica4.restar()
+
+  print('Quinto objeto')
+  aritmetica5 = Aritmetica(operando2=5, operando1=8)
+  aritmetica5.sumar()
