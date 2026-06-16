640a68d8714fff5640a6a0e75d13b4832eadd5ce
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-16 21:20:16 +0100

    Ejemplo de encapsulamiento - Parte 2

--- coche.py
+++ coche.py
@@ -1,24 +1,45 @@
 class Coche:
 
   def __init__(self, marca, modelo, color):
-    self.marca = marca       # Atributo público
-    self._modelo = modelo    # Atributo protegido
-    self.__color = color     # Atributo privado
+    self._marca = marca
+    self._modelo = modelo
+    self._color = color
 
   def conducir(self):
     print(f'''Conduciendo el coche:
-          Marca: {self.marca}
+          Marca: {self._marca}
           Modelo: {self._modelo}
-          Color: {self.__color}
-''')
+          Color: {self._color}''')
+    
+  def get_marca(self):
+    return self._marca
+  
+  def set_marca(self, marca):
+    self._marca = marca
+
+  def get_modelo(self):
+    return self._modelo
+  
+  def set_modelo(self, modelo):
+    self._modelo = modelo
+
+  def get_color(self):
+    return self._color
+  
+  def set_color(self, color):
+    self._color = color
 
 # Programa principal
 if __name__ == '__main__':
   coche1 = Coche('Toyota', 'Yaris', 'Azul')
   coche1.conducir()
 
-  coche1.marca = 'Toyota 2'
-  coche1._modelo = 'Yaris 2'       # Esto no es una buena práctica
-  coche1.__color = 'Azul 2'        # Ignora la modificación
-  coche1._Coche__color = 'Azul 3'  # Es una mala práctica
+  # Utilizar el concepto de encapsulamiento (get/set)
+  coche1.set_marca('Toyota 2')
+  coche1.set_modelo('Yaris 2')
+  coche1.set_color('Azul 2')
   coche1.conducir()
+  print(f'Atributos del coche1: {coche1.__dict__}')
+
+  coche2 = Coche('Chevrolet', 'Trax', 'Negro')
+  print(f'Atributos del coche2: {coche2.__dict__}')
