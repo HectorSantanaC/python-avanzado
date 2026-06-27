2bfb666f57f475e00667b7d3f0559f19caca08ea
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-27 19:22:35 +0100

    Leer un archico en Python

--- ManejoArchivos/crear_archivo_modo_exclusivo.py
+++ ManejoArchivos/crear_archivo_modo_exclusivo.py
@@ -4,7 +4,8 @@ nombre_archivo = 'mi_archivo.txt'
 
 try:
   with open (nombre_archivo, 'x') as archivo:
-    archivo.write('Escritura en modo exclusivo')
+    archivo.write('Escritura en modo exclusivo\n')
+    archivo.write('Escritura en modo exclusivo 2\n')
   print(f'Se ha creado el archivo {nombre_archivo}')
 
 except FileExistsError as e:
---
+++ ManejoArchivos/leer_archivo.py
@@ -0,0 +1,19 @@
+print('Leer archivo con Python')
+
+nombre_archivo = 'mi_archivo.txt'
+
+# Leer un archivo usando el método readLines
+with open(nombre_archivo, 'r') as archivo:
+
+  # Leer todas las líneas del archivo
+  # print(archivo.readlines())
+  lineas = archivo.readlines()
+  for linea in lineas:
+    print(linea.strip())
+
+# Leer todo el contenido del archivo usando el método read
+
+print('Leyendo el contenido con el método read')
+
+with open(nombre_archivo, 'r') as archivo:
+  print(archivo.read())
