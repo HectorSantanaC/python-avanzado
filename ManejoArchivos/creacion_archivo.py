d35f8bd6152e527fecc707160206d2a1bb5826d7
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-27 19:04:18 +0100

    Bloque with en Python

--- ManejoArchivos/creacion_archivo.py
+++ ManejoArchivos/creacion_archivo.py
@@ -1,10 +1,15 @@
 # Crear Archivo
 nombre_archivo = 'mi_archivo.txt'
 
+#Abrir el archivo en modo escritura ('w')
+with open(nombre_archivo, 'w') as archivo:
+  archivo.write('Hola ¿Como estás?\n')
+  archivo.write('Estoy agregando información al archivo\n')
+
 # Abrir archivo
-archivo = open(nombre_archivo, 'w')
-archivo.write('Hola ¿Como estás?\n')
-archivo.write('Estoy agregando información al archivo\n')
-archivo.close()
+# archivo = open(nombre_archivo, 'w')
+# archivo.write('Hola ¿Como estás?\n')
+# archivo.write('Estoy agregando información al archivo\n')
+# archivo.close()
 
 print(f'Se creó el archivo: {nombre_archivo}')
