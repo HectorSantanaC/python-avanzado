761a6982da4a3ddbae84bb87e3bbf06760fdf28b
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-27 19:12:39 +0100

    Creación archivo en modo exclusivo

---
+++ ManejoArchivos/crear_archivo_modo_exclusivo.py
@@ -0,0 +1,12 @@
+print('*** Abrir un archivo en modo exclusivo ***')
+
+nombre_archivo = 'mi_archivo.txt'
+
+try:
+  with open (nombre_archivo, 'x') as archivo:
+    archivo.write('Escritura en modo exclusivo')
+  print(f'Se ha creado el archivo {nombre_archivo}')
+
+except FileExistsError as e:
+  print(f'El archivo {nombre_archivo} ya existe')
+  print(f'Detalle del error: {e}')
