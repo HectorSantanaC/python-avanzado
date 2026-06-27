127673739a001806035bd5642e4643157ab2d7bb
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-27 18:58:42 +0100

    Creación de archivos en python

---
+++ ManejoArchivos/creacion_archivo.py
@@ -0,0 +1,10 @@
+# Crear Archivo
+nombre_archivo = 'mi_archivo.txt'
+
+# Abrir archivo
+archivo = open(nombre_archivo, 'w')
+archivo.write('Hola ¿Como estás?\n')
+archivo.write('Estoy agregando información al archivo\n')
+archivo.close()
+
+print(f'Se creó el archivo: {nombre_archivo}')
