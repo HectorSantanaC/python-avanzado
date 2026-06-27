3bd7fb8a435a5fb96d2944285dad965331a22292
HectorSantanaC <hector.santana.gc@gmail.com>
2026-06-27 20:23:10 +0100

    Cliente máquina snacks - parte 1

---
+++ ManejoArchivos/maquina_snack_proyecto/maquina_snacks_app.py
@@ -0,0 +1,52 @@
+from ManejoArchivos.maquina_snack_proyecto.servicio_snacks import ServicioSnacks
+
+
+class MaquinaSnacks:
+  def __init__(self):
+    self.servicio_snacks = ServicioSnacks()
+    self.productos = []
+
+  def maquina_snacks(self):
+    salir = False
+    print('*** Máquina de Snacks ***')
+    self.servicio_snacks.mostrar_snacks()
+    
+    while not salir:
+      try:
+        opcion = self.mostrar_menu()
+        salir = self.ejecutar_opcion(opcion)
+
+      except Exception as e:
+        print(f'Ocurrió un error: {e}')
+
+  def mostrar_menu(self):
+    print(f'''Menu:
+          1. Comprar snack
+          2. Mostrar ticket
+          3. Agregar nuevo snack al inventario
+          4. Inventario de snacks
+          5. Salir''')
+    
+    return int(input('Elige una opción: '))
+  
+  def ejecutar_opcion(self, opcion):
+    if opcion == 1:
+      self.comprar_snack()
+
+    elif opcion == 2:
+      self.mostrar_ticket()
+
+    elif opcion == 3:
+      self.agregar_snack()
+
+    elif opcion == 4:
+      self.servicio_snacks.mostrar_snacks()
+
+    elif opcion == 5:
+      print('¡Regresa pronto!')
+      return True
+    
+    else:
+      print(f'Opción inválida: {opcion}')
+      
+    return False
