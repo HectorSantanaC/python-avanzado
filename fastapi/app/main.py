d428d9512ba6abfddbf92f28e2da32fd6b106350
Héctor Santana <hector.santana.gc@gmail.com>
2026-07-16 20:20:51 +0100

    Crear la dependencia de base de datos y enpoints personalizados

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+import crud, schemas
+from database import get_db
+
 
 app = FastAPI()
 
@@ -10,21 +14,27 @@ class Producto(BaseModel):
 
 productos = []
 
-@app.get("/productos")
-def listar_productos():
-  return {"productos": productos }
+@app.get("/productos", response_model=list[schemas.ProductoResponse])
+def listar_productos(db:Session = Depends(get_db)):
+  return crud.obtener_productos(db)
 
-@app.post("/productos")
-def agregar_producto(producto: Producto):
-  productos.append(producto)
-  return {"mensaje": "Producto agregado", "producto": producto}
+@app.post("/productos", response_model=schemas.ProductoCreate)
+def agregar_producto(producto: schemas.ProductoCreate, db: Session = Depends(get_db)):
+  return crud.crear_producto(db, producto)
 
-@app.put("/productos/{id}")
-def actualizar_producto(id: int, nombre: str):
-  productos[id] = nombre
-  return {"mensaje": "Producto actualizado", "producto": nombre}
+@app.put("/productos/{id}", response_model=list[schemas.ProductoResponse])
+def actualizar_producto(producto_id: int, datos: schemas.ProductoCreate, db: Session = Depends(get_db)):
+  producto = crud.actualizar_producto(db, producto_id, datos)
+
+  if not producto:
+    raise HTTPException(status_code=404, detail="Producto no encontrado")
+  
+  return producto
 
 @app.delete("/productos/{id}")
-def eliminar_producto(id: int):
-  eliminado = productos.pop(id)
-  return {"mensaje": "Producto eliminado", "producto": eliminado}
+def eliminar_producto(producto_id: int, db: Session = Depends(get_db)):
+  producto = crud.eliminar_producto(db, producto_id)
+
+  if not producto:
+    raise HTTPException(status_code=404, detail="Producto no encontrado")
+  return {"mensaje": "Producto eliminado"}
