f2bf323daf7c9490c0835166ec96b9e5e674c6b8
Héctor Santana <hector.santana.gc@gmail.com>
2026-07-19 18:21:16 +0100

    Rutas

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,51 +1,6 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
-import crud, schemas
-from database import get_db
-from deps import require_admin
-
+from fastapi import FastAPI
+from api.v1.api import api_router
 
 app = FastAPI()
 
-### Producto ###
-
-class Producto(BaseModel):
-  nombre: str
-  precio: float
-  en_stock: bool
-
-@app.get("/productos", response_model=list[schemas.ProductoResponse])
-def listar_productos(db:Session = Depends(get_db)):
-  return crud.obtener_productos(db)
-
-@app.post("/productos", response_model=schemas.ProductoCreate, dependencies=[Depends(require_admin)])
-def agregar_producto(producto: schemas.ProductoCreate, db: Session = Depends(get_db)):
-  return crud.crear_producto(db, producto)
-
-@app.put("/productos/{id}", response_model=list[schemas.ProductoResponse])
-def actualizar_producto(producto_id: int, datos: schemas.ProductoCreate, db: Session = Depends(get_db)):
-  producto = crud.actualizar_producto(db, producto_id, datos)
-
-  if not producto:
-    raise HTTPException(status_code=404, detail="Producto no encontrado")
-  
-  return producto
-
-@app.delete("/productos/{id}")
-def eliminar_producto(producto_id: int, db: Session = Depends(get_db)):
-  producto = crud.eliminar_producto(db, producto_id)
-
-  if not producto:
-    raise HTTPException(status_code=404, detail="Producto no encontrado")
-  return {"mensaje": "Producto eliminado"}
-
-### Categoría ###
-
-@app.post("/categorias", response_model=schemas.CategoriaResponse)
-def crear_categoria(categoria: schemas.CategoriaCreate, db: Session = Depends(get_db)):
-  return crud.crear_categoria(db, categoria)
-
-@app.get("/categorias", response_model=list[schemas.CategoriaResponse])
-def listar_categoria(db: Session = Depends(get_db)):
-  return crud.obtener_categorias(db)
+app.include_router(api_router, prefix="api/v1")
