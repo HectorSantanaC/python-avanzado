3f97714b698df417fec572c209513a7f346e78d4
Héctor Santana <hector.santana.gc@gmail.com>
2026-07-21 17:50:42 +0100

    Crear rutas

--- fastapi/app/api/v1/pedido.py
+++ fastapi/app/api/v1/pedido.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import Session
 from deps.deps import get_db, get_current_user
 from crud import pedido as crud_pedido
 
-router = APIRouter()
+api_router = APIRouter()
 
-@router.post("/confirmar")
+@api_router.post("/confirmar")
 def confirmar_pedido(db: Session = Depends(get_db), user = Depends(get_current_user)):
   try:
     pedido = crud_pedido.crear_pedido(db, user.id)
