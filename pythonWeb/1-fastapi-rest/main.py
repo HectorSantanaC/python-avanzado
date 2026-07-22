475f562062f908f44a32ebb2b884a6e12e00e158
Héctor Santana <hector.santana.gc@gmail.com>
2026-07-22 18:32:00 +0100

    Endpoint que devuelve una lista de mensajes (dicts)

--- pythonWeb/1-fastapi-rest/main.py
+++ pythonWeb/1-fastapi-rest/main.py
@@ -2,8 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+sample_messages: list[dict] =  [
+  {
+    'id': 1,
+    'text': 'Hola Mundo Python con fastAPI'
+  },
+  {
+    'id': 2,
+    'text': 'Sección de fastAPI en progreso...'
+  },
+  {
+    'id': 3,
+    'text': 'Este es un mensaje de prueba!'
+  },
+]
+
 @app.get('/')
 def read_data():
   return {
     "message": 'Hola mundo! con FastAPI mi primer endponint!!!'
   }
+
+@app.get('/messages')
+def list_messages():
+  return sample_messages
