4db89a4e9e8ccc03ba2db1dff88e1857885c83eb
Héctor Santana <hector.santana.gc@gmail.com>
2026-07-22 18:47:03 +0100

    Modelos Pydantic: creando la clase Message

--- pythonWeb/1-fastapi-rest/main.py
+++ pythonWeb/1-fastapi-rest/main.py
@@ -1,20 +1,17 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Message(BaseModel):
+    id: int
+    text: str
 
 app = FastAPI()
 
-sample_messages: list[dict] =  [
-  {
-    'id': 1,
-    'text': 'Hola Mundo Python con fastAPI'
-  },
-  {
-    'id': 2,
-    'text': 'Sección de fastAPI en progreso...'
-  },
-  {
-    'id': 3,
-    'text': 'Este es un mensaje de prueba!'
-  },
+sample_messages: List[Message] =  [
+  Message(id=1, text='Hola Mundo Python con fastAPI'),
+  Message(id=2, text='Sección de fastAPI en progreso...'),
+  Message(id=3, text='Este es un mensaje de prueba!')
 ]
 
 @app.get('/')
@@ -23,6 +20,6 @@ def read_data():
     "message": 'Hola mundo! con FastAPI mi primer endponint!!!'
   }
 
-@app.get('/messages')
+@app.get('/messages', response_model=List[Message])
 def list_messages():
   return sample_messages
